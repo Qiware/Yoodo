56a4de48fa2e0b19f94c800f58893ae4f9f08b3b
qifengzou <qifengzou@tencent.com>
2023-07-27 09:18:30 +0800

    模型训练
    将模型训练和趋势预测分离

--- src/train/trainer.py
+++ src/train/trainer.py
@@ -24,7 +24,7 @@ GET_TRANSACTION_NUM = 1000
 TRAIN_DATA_TRANSACTION_NUM = 30
 
 # 股票预测
-class Predicter():
+class Trainer():
     def __init__(self):
         # 连接数据库
         self.database = Database()
@@ -137,11 +137,11 @@ class Predicter():
         feature_train, feature_test, target_train, target_test = train_test_split(feature, target, test_size=0.05, random_state=1)
 
         # 创建线性回归对象
-        #predict_model = LinearRegression()
-        #predict_model.fit(feature_train, target_train) # 训练
-
-        predict_model = MLPRegressor(hidden_layer_sizes=(100,), activation='relu', solver='adam', alpha=0.0001, batch_size='auto', learning_rate='constant', learning_rate_init=0.001, power_t=0.5, max_iter=200, shuffle=True, random_state=None, tol=0.0001, verbose=False, warm_start=False, momentum=0.9, nesterovs_momentum=True, early_stopping=False, validation_fraction=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08, n_iter_no_change=10, max_fun=15000)
+        predict_model = LinearRegression()
         predict_model.fit(feature_train, target_train) # 训练
+
+        #predict_model = MLPRegressor(hidden_layer_sizes=(100,), activation='relu', solver='adam', alpha=0.0001, batch_size='auto', learning_rate='constant', learning_rate_init=0.001, power_t=0.5, max_iter=200, shuffle=True, random_state=None, tol=0.0001, verbose=False, warm_start=False, momentum=0.9, nesterovs_momentum=True, early_stopping=False, validation_fraction=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08, n_iter_no_change=10, max_fun=15000)
+        #predict_model.fit(feature_train, target_train) # 训练
 
         # 模型评估
         predict_test = predict_model.predict(feature_test)
@@ -168,15 +168,12 @@ if __name__ == "__main__":
 
     log_init("../../log/predicter.log")
 
-    predict = Predicter()
+    trainer = Trainer()
 
     date = sys.argv[1]
 
     # 生成训练数据
-    # predict.gen_train_data(date)
+    # trainer.gen_train_data(date)
 
     # 进行模型训练
-    predict.train(date)
-
-    # 进行结果预测
-    # predict.predit()
+    trainer.train(date)
