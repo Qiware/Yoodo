627dbfc13ff9bc5c5da639dd11b6939c12f096bf
gsp412 <guoshuangpeng@le.com>
2023-09-21 21:22:24 +0800

    style: code format

--- src/server/trainer/logic/trainer.py
+++ src/server/trainer/logic/trainer.py
@@ -16,20 +16,23 @@ from classifier import Classifier
 
 sys.path.append("../../repo/data")
 from data import Data
+
 sys.path.append("../../repo/lib/log")
 from log import *
 
 # 拉取训练交易数据条目
 GET_TRANSACTION_MAX_NUM = 1000
 
+
 # 股票预测
-class Trainer():
+class Trainer(object):
     def __init__(self, model_type, days, is_build=False):
-        ''' 初始化
+        """
+        初始化
             @Param model_type: 训练模型(r:回归模型; c:分类模型)
             @Param days: 预测周期
             @Param is_build: 是否重建
-        '''
+        """
         # 指定数据源
         self.data = Data()
         self.scaler = StandardScaler()
@@ -40,20 +43,20 @@ class Trainer():
             self.model = Classifier(days, is_build)
 
     def train(self, date, days, is_build=False):
-        ''' 模型训练 '''
+        """ 模型训练 """
         # 加载训练数据
         feature, target = self.data.load_train_data(date, days)
 
         # 划分训练集和测试集
         x_train, x_test, y_train, y_test = train_test_split(
-                feature, target, test_size=0.001, random_state=1)
+            feature, target, test_size=0.001, random_state=1)
 
         # 训练模型
         x_train_scaled = self.scaler.fit_transform(x_train)
 
-        self.model.fit(x_train_scaled, y_train) # 训练
+        self.model.fit(x_train_scaled, y_train)  # 训练
 
-        self.model.dump() # 固化模型
+        self.model.dump()  # 固化模型
 
         # 模型评估
         x_test_scaled = self.scaler.transform(x_test)
@@ -77,10 +80,12 @@ class Trainer():
                 stock_key = stock["stock_key"]
 
                 # 加载特征数据
-                base_date, feature = self.data.load_feature(stock_key, date, days)
+                base_date, feature = self.data.load_feature(stock_key, date,
+                                                            days)
                 if feature is None:
-                    logging.error("Load feature failed! stock_key:%s date:%s days:%d",
-                                 stock_key, date, days)
+                    logging.error(
+                        "Load feature failed! stock_key:%s date:%s days:%d",
+                        stock_key, date, days)
                     continue
 
                 # 进行结果预测
@@ -88,11 +93,14 @@ class Trainer():
 
                 ratio = self.model.predict(feature_scaled)
 
-                print("predict: %s %s %s %s %s" % (stock_key, date, days, ratio[0], stock["name"]))
-                logging.info("predict: %s %s %s %s %s", stock_key, date, days, ratio[0], stock["name"])
+                print("predict: %s %s %s %s %s" % (
+                stock_key, date, days, ratio[0], stock["name"]))
+                logging.info("predict: %s %s %s %s %s", stock_key, date, days,
+                             ratio[0], stock["name"])
 
                 # 更新预测结果
-                self.data.update_predict(stock_key, date, days, base_date, float(ratio[0]))
+                self.data.update_predict(stock_key, date, days, base_date,
+                                         float(ratio[0]))
 
         print('R2值为：', r2)
         print('MSE值为：', mse)
@@ -100,18 +108,20 @@ class Trainer():
         '''结果可视化'''
         xy = range(0, len(y_test))
         plt.figure(figsize=(4, 3))
-        plt.scatter(xy, y_test,color="red",label="Sample Point",linewidth=3)
-        plt.plot(xy, y_predict,color="orange",label="Predict line",linewidth=2)
+        plt.scatter(xy, y_test, color="red", label="Sample Point", linewidth=3)
+        plt.plot(xy, y_predict, color="orange", label="Predict line",
+                 linewidth=2)
         plt.legend()
         plt.show()
 
         return None
 
     def build(self, date, days):
-        ''' 构建训练模型 '''
+        """ 构建训练模型 """
 
         # 生成训练数据
-        self.data.gen_train_data(self.model_type, date, days, GET_TRANSACTION_MAX_NUM)
+        self.data.gen_train_data(self.model_type, date, days,
+                                 GET_TRANSACTION_MAX_NUM)
 
         # 进行模型训练
         self.train(date, days, True)
--- src/server/trainer/main.py
+++ src/server/trainer/main.py
@@ -10,18 +10,17 @@ from sklearn.model_selection import train_test_split
 
 sys.path.append("../../repo/lib/log")
 from log import *
-sys.path.append("../../server/trainer/logic")
 from logic.trainer import Trainer
 
-from trainer import *
 
 def usage():
-    ''' 展示帮助信息 '''
+    """ 展示帮助信息 """
     print("python3 ./main.py [model] [action] [date] [days]")
     print("     - model: 训练模型(r:线性模型; c:分类模型)")
     print("     - action: 操作行为(update:更新模型; rebuild:重建模型)")
     print("     - date: 预测时间")
     print("     - days: 预测周期")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
@@ -29,15 +28,15 @@ if __name__ == "__main__":
         exit(-1)
 
     # 提取参数
-    model = sys.argv[1] # 训练模型(r:线性模型 c:分类模型)
-    action = sys.argv[2] # 操作行为(update:增量更新 rebuild:模型重建)
-    date = sys.argv[3] # 训练数据截止日期(格式: YYYYMMDD)
-    days = int(sys.argv[4]) # 训练模型预测周期(时间不宜过长)
+    model = sys.argv[1]         # 训练模型(r:线性模型 c:分类模型)
+    action = sys.argv[2]        # 操作行为(update:增量更新 rebuild:模型重建)
+    date = sys.argv[3]          # 训练数据截止日期(格式: YYYYMMDD)
+    days = int(sys.argv[4])     # 训练模型预测周期(时间不宜过长)
 
     # 初始化日志
     log_init("../../../log/trainer.log")
 
-    trainer = Trainer(model, days, action=="build")
+    trainer = Trainer(model, days, action == "build")
     if action == "build":
         print("Build model. date:%s days:%s" % (date, days))
         trainer.build(date, days)
